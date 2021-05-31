e6d8a8496cd3447d380a49fc8b1a6023aaaf1ed2
Todd Pressley <todd.pressley1@gmail.com>
2021-05-30 22:04:15 -0400

    run migrations and create a posts scaffold

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -14,6 +14,10 @@ import BlogLayout from './layouts/BlogLayout/BlogLayout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/posts/new" page={NewPostPage} name="newPost" />
+      <Route path="/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
+      <Route path="/posts/{id:Int}" page={PostPage} name="post" />
+      <Route path="/posts" page={PostsPage} name="posts" />
       <Set wrap={BlogLayout}>
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
